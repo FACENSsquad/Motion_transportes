28d161dc6a2ef123aed7b8e326415f1f098a105c
Vinicius Domingues Silva <91794441+ViniciusDSS@users.noreply.github.com>
2022-09-01 11:36:19 -0300

    Conexao
    Conexão com o banco de dados

--- Motion_Logistica/src/motion_logistica/Motion_Logistica.java
+++ Motion_Logistica/src/motion_logistica/Motion_Logistica.java
@@ -1,13 +1,16 @@
 
 package motion_logistica;
 
+import CONEXAO.Conexao;
+
 
 public class Motion_Logistica {
 
     
     public static void main(String[] args) {
         
-        
+        Conexao conec = new Conexao();
+        conec.conectaBanco();
         
     }
     
