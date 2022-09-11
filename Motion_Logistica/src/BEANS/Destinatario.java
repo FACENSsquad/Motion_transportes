16396d3b7191ab54f718ac905103119e3b08cdeb
Vinicius Domingues Silva <91794441+ViniciusDSS@users.noreply.github.com>
2022-09-11 20:50:49 -0300

    Class destinatario

---
+++ Motion_Logistica/src/BEANS/Destinatario.java
@@ -0,0 +1,85 @@
+
+package BEANS;
+
+/* 
+cnpj varchar(14) unique,
+    nome_fantasia varchar(250),
+    endereco varchar(250),
+    uf varchar(2),
+    cidade varchar(70),
+    ddd varchar(2) ,
+    telefone varchar(12) unique ,
+    email varchar(150) unique not null,
+
+
+*/
+public class Destinatario {
+    
+    private String nome;
+    private String endereco;
+    private String uf;
+    private String cidade;
+    private String ddd;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}
