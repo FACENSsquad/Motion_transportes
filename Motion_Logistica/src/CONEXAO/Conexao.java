be244e41d4aa21729e98c161b78d3198cb017a49
Vinicius Domingues Silva <91794441+ViniciusDSS@users.noreply.github.com>
2022-09-21 20:51:53 -0300

    att

--- Motion_Logistica/src/CONEXAO/Conexao.java
+++ Motion_Logistica/src/CONEXAO/Conexao.java
@@ -2,77 +2,33 @@
 package CONEXAO;
 
 import java.sql.Connection;
-import java.sql.Statement;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
+
+
 
 public class Conexao {
     
-    //atributos de conexão com o banco
-    private Connection conn = null; //variável de conexão com o banco
-    private Statement statement; //variável de manipulação do SQL
-    private ResultSet resultSet;
-    
-    private String servidor = "localhost:3306";
-    private String nomeDoBanco = "mt_logistica";
-    private String usuario = "root";
-    private String senha = "";
-    
-    //Construtor    
-    public Conexao(){
-    
-    }
-    
-        public Conexao(String servidor, String nomeDoBanco, String usuario, String senha){
-        this.servidor = servidor;
-        this.nomeDoBanco = nomeDoBanco;
-        this.usuario = usuario;
-        this.senha = senha;
-    }
- 
-       public Connection getConn() {
-        return conn;
-    }
+   Connection conn;
+    public Connection mt_Conexao(){
+        try{
+            // Tentar estabelecer conexão
 
-    public void setConn(Connection conn) {
-        this.conn = conn;
-    }
-
-    public Statement getStatement() {
-        return statement;
-    }
-
-    public void setStatement(Statement statement) {
-        this.statement = statement;
-    }
-
-    public ResultSet getResultSet() {
-        return resultSet;
-    }
-
-    public void setResultSet(ResultSet resultSet) {
-        this.resultSet = resultSet;
-    }
-        
-    public void conectaBanco(){
-        try {
-            //Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-            conn = DriverManager.getConnection("jdbc:mysql://" + servidor + "/" + nomeDoBanco, usuario, senha);
-            //conn = DriverManager.getConnection("jdbc:mysql://localhost3306/banco_loja", "root", "root");
-            
-            if(conn != null){
-                System.out.println("Conexão efetuada com sucesso! " + "ID: " + conn);
-            }            
-            
-        } catch (Exception e) {
-            System.out.println("Conexão não realizada - ERRO: " + e.getMessage());
+           conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mt_logistica?serverTimezone=UTC",  // linha de conexão
+                "root", // usuario do MySQL
+                "" // senha do MySQL
+            );
+            return conn;
+        } 
+        catch(Exception e){
+            // Se der erro exiba:
+            System.out.println("Erro ao conectar"+ e.getMessage());
+            return null;
         }
     }
     
-    public boolean fechaBanco(){
+    public boolean fecha_mt(){
+        
         try {
             conn.close();
             return true;
@@ -82,49 +38,7 @@ public class Conexao {
         }
     }
     
-    public int insertSQL(String SQL){
-        int status = 0;
-        try {
-            //createStatement de con para criar o Statement
-            this.setStatement(getConn().createStatement());            
-
-            // Definido o Statement, executamos a query no banco de dados
-            this.getStatement().executeUpdate(SQL);            
-        
-            return status;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return status;
-        }
-    }
-       
-    public void executarSQL(String sql) {
-        try {
-            this.statement = conn.createStatement(
-                    ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            this.resultSet = this.statement.executeQuery(sql);
-
-//            while (this.getResultSet().next()) {
-//                System.out.println(this.getResultSet().getInt(1));
-//            }
-        } catch (SQLException sqlex) {
-            sqlex.printStackTrace();
-        }
-    }
-       
-    public boolean updateSQL(String pSQL){
-        try {            
-            //createStatement de con para criar o Statement
-            this.setStatement(getConn().createStatement());
-
-            // Definido o Statement, executamos a query no banco de dados
-            getStatement().executeUpdate(pSQL);
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return false;
-        }
-        return true;
-    } 
-        
 }
+    
+     
+      
